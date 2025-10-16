082e671c47d4fe185ab70a9d8cee06609a661b1f
qunhui201 <qunhui201@outlook.com>
2025-10-16 16:41:53 +0800

    Update build_m3u.py

--- md/build_m3u.py
+++ md/build_m3u.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 
 # -------- 配置 ---------
 REMOTE_FILES = [
-    "http://iptv.catvod.com/tv.m3u",
     "http://httop.top/iptvs.m3u",
     "http://httop.top/iptvx.m3u"
 ]
